6f29c432ce5d253c6d2c9a492af439abd3af9ad0
RobinSuthar <152766032+RobinSuthar@users.noreply.github.com>
2024-10-30 23:55:49 -0600

    Hurray

--- app/index.js
+++ app/index.js
@@ -1,23 +1,27 @@
-
-
-import React from 'react';
+import React, { useState } from "react";
 import {
   SafeAreaView,
   StyleSheet,
   View,
   TextInput,
-  Button
-} from 'react-native';
-import ToDoList from '../components/ToDoList';
-import ToDoForm from '../components/ToDoForm';
-
+  Button,
+} from "react-native";
+import ToDoList from "../components/ToDoList";
+import ToDoForm from "../components/ToDoForm";
 
 function App() {
+  const [tasks] = useState([
+    "Hurray!! One more month",
+    "Do laundry",
+    "Go to gym",
+    "Walk dog",
+    "ToDoLabs",
+  ]);
   return (
-      <SafeAreaView>
-        <ToDoList />
-        <ToDoForm></ToDoForm>
-      </SafeAreaView>
+    <SafeAreaView>
+      <ToDoList tasks={tasks} />
+      <ToDoForm></ToDoForm>
+    </SafeAreaView>
   );
 }
 
